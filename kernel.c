7261a5ede2f496c0c640708bdce5b99e331555a2
Indra F Nugroho <13518016@std.stei.itb.ac.id>
2020-02-12 15:37:05 +0700

    initial commit

---
+++ kernel.c
@@ -0,0 +1,12 @@
+int main () {
+	putInMemory(0xB000, 0x8000, 'H');
+	putInMemory(0xB000, 0x8001, 0xD);
+	putInMemory(0xB000, 0x8002, 'a');
+	putInMemory(0xB000, 0x8003, 0xD);
+	putInMemory(0xB000, 0x8004, 'i');
+	putInMemory(0xB000, 0x8005, 0xD);
+
+	while(1);
+}
+
+void handleInterrupt21 (int AX, int BX, int CX, int DX) {}
