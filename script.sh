8c0ff771b45d678cced2414e1750a9db30110375
Indra F Nugroho <13518016@std.stei.itb.ac.id>
2020-02-12 20:29:53 +0700

    add script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+dd if=/dev/zero of=system.img bs=512 count=2880
+nasm bootloader.asm -o bootloader
+dd if=bootloader of=system.img bs=512 count=1 conv=notrunc
+dd if=map.img of=system.img bs=512 count=1 seek=1 conv=notrunc
+dd if=dir.img of=system.img bs=512 count=1 seek=2 conv=notrunc
+bcc -ansi -c -o kernel.o kernel.c
+nasm -f as86 kernel.asm -o kernel_asm.o
+ld86 -o kernel -d kernel.o kernel_asm.o
+dd if=kernel of=system.img bs=512 conv=notrunc seek=3
