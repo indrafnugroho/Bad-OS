ab9ef0170dfcb0bd573fb3b3e61faadb3e2dac39
Indra F Nugroho <13518016@std.stei.itb.ac.id>
2020-02-12 20:26:25 +0700

    add printstring

--- kernel.c
+++ kernel.c
@@ -1,12 +1,81 @@
-int main () {
-	putInMemory(0xB000, 0x8000, 'H');
-	putInMemory(0xB000, 0x8001, 0xD);
-	putInMemory(0xB000, 0x8002, 'a');
-	putInMemory(0xB000, 0x8003, 0xD);
-	putInMemory(0xB000, 0x8004, 'i');
-	putInMemory(0xB000, 0x8005, 0xD);
+/* Ini deklarasi fungsi */
+void handleInterrupt21 (int AX, int BX, int CX, int DX);
+void printString(char *string);
+void readString(char *string);
+void readSector(char *buffer, int sector);
+void writeSector(char *buffer, int sector);
+void readFile(char *buffer, char *filename, int *success);
+void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+void writeFile(char *buffer, char *filename, int *sectors);
+void executeProgram(char *filename, int segment, int *success);
 
-	while(1);
+int main() {
+	makeInterrupt21();
+	printString("Testestestes");
+	while (1);
 }
 
-void handleInterrupt21 (int AX, int BX, int CX, int DX) {}
+void handleInterrupt21 (int AX, int BX, int CX, int DX) {
+	switch (AX) {
+		case 0x0:
+			printString(BX);
+			break;
+		case 0x1:
+			readString(BX);
+			break;
+		case 0x2:
+			readSector(BX, CX);
+			break;
+		case 0x3:
+			writeSector(BX, CX);
+			break;
+		case 0x4:
+			readFile(BX, CX, DX);
+			break;
+		case 0x5:
+			writeFile(BX, CX, DX);
+			break;
+		case 0x6:
+			executeProgram(BX, CX, DX);
+			break;
+		default:
+			printString("Invalid interrupt");
+	}
+}
+
+// Implementasi fungsi
+void printString(char *string) { //WORK!!!
+	int i = 0;
+	while(string[i] != '\0'){
+		interrupt(0x10, 0xE00 + string[i], 0, 0, 0);
+		i++;
+	}
+}
+
+void readString(char *string) {
+
+}
+
+void readSector(char *buffer, int sector) {
+
+}
+
+void writeSector(char *buffer, int sector) {
+
+}
+
+void readFile(char *buffer, char *filename, int *success) {
+
+}
+
+void clear(char *buffer, int length) { //Fungsi untuk mengisi buffer dengan 0
+
+} 
+
+void writeFile(char *buffer, char *filename, int *sectors) {
+
+}
+
+void executeProgram(char *filename, int segment, int *success) {
+
+}
