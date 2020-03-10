744d7fdd9219536f940d1edf485b3cf5c78a50e3
faridlazuarda <faridadilazuarda29@gmail.com>
2020-03-10 19:25:02 +0700

    update interrupt

--- milestone1/kernel.c
+++ milestone1/kernel.c
@@ -31,33 +31,38 @@ int main() {
 	while (1);
 }
 
+
 void handleInterrupt21 (int AX, int BX, int CX, int DX) {
-	switch (AX) {
-		case 0x0:
+	char AL, AH;
+	AL = (char) (AX);
+	AH = (char) (AX >> 8);
+	switch (AL) {
+		case 0x00:
 			printString(BX);
 			break;
-		case 0x1:
+		case 0x01:
 			readString(BX);
 			break;
-		case 0x2:
+		case 0x02:
 			readSector(BX, CX);
 			break;
-		case 0x3:
+		case 0x03:
 			writeSector(BX, CX);
 			break;
-		case 0x4:
-			readFile(BX, CX, DX);
+		case 0x04:
+			readFile(BX, CX, DX, AH);
 			break;
-		case 0x5:
-			writeFile(BX, CX, DX);
+		case 0x05:
+			writeFile(BX, CX, DX, AH);
 			break;
-		case 0x6:
-			executeProgram(BX, CX, DX);
-			break;
+		case 0x06:
+			executeProgram(BX, CX, DX, AH);
+			Break;
 		default:
 			printString("Invalid interrupt");
 	}
 }
+
 
 // Implementasi fungsi
 void printString(char *string) {
