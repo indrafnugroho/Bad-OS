d05795d2a7b4db85ec46b2e98c38ea65628b364f
yulizar57 <anandayulizar@gmail.com>
2020-03-13 20:13:58 +0700

    shell tidak selesai bro error tidak jelas

--- milestone2/shell.c
+++ milestone2/shell.c
@@ -11,12 +11,88 @@ int main() {
 			}
 			interrupt(0x21, 0x00, ">", 0, 0);
 			interrupt(0x21, 0x01, input, 1, 0);
-		} while (input == "");
+		} while (compareStr(input, ""));
 
 		interrupt(0x21, 0x00, "lewat\r\n", 0, 0);
 		interrupt(0x21, 0x00, input, 0, 0);
+		curdir = searchForPath(&input, 0xFF);
+
+		
 	}
 
 	return 0;
 }
 
+int compareStr(char* strA, char* strB) {
+	int i = 0;
+	while (!(strA[i] == '\0' && strB[i] == '\0')) {
+		if (strA[i] != strB[i]) {
+            return 0;
+        }
+		++i;
+	}
+	return 1;
+}
+
+char searchForPath(char* path, char parentIndex) {
+	char files[1024];
+	char tempBuffer[512];
+	char tempBuffer2[512];
+	int isFound = 0;
+	int isNameMatch, k, s, j, idxName;
+	int h, l;
+
+	interrupt(0x21, 0x2, files, 0x101, 0);
+	for (k=512; k < 1024; k++) {
+		files[k] = tempBuffer[k-512];
+	}
+	k = 0;
+	while(!isFound) {
+		//search for parent idx with matching path name
+		for (k; k < 1024; k+=16) {
+			if (files[k] == parentIndex) {
+				idxName = k+2;
+				if (files[idxName] != 0x0 && files[k+1] != 0xFF) {
+					//matching name
+					isNameMatch = 1;
+					for (h=0; h < 14; h++) {
+						if (path[h] != files[idxName + h]) {
+							isNameMatch = 0;
+							break;
+						}
+					} 
+					
+					if (isNameMatch) {
+						isFound = 1;
+						s = files[k+1]; //in hexa gengs
+						break;
+					}
+				}
+			}
+		}
+		if (k==1024) break; // break while terluar
+	}
+
+	if (!isFound) {
+		parentIndex = 0x00;
+	}
+
+	return parentIndex;
+}
+
+char* searchName(char parentIndex) {
+	char files[1024];
+	char name[14];
+	
+	int isFound = 0;
+	int isNameMatch, k, s, j, idxName;
+	int h, l;
+
+	interrupt(0x21, 0x2, files, 0x101, 0);
+	idxName = parentIndex + 2;
+	while (files[idxName] != '\0') {
+		name[idxName - 2] = files[idxName];
+	}
+
+	return name;
+}
